662f6be3f3e129184b5f402b50bb4149ab35b66e
Santiago Saavedra <ssaavedraa@santiagosaavedra.com.co>
2024-01-23 18:36:02 -0500

    fixed icon properties

--- src/components/icons/ErrorIcon.tsx
+++ src/components/icons/ErrorIcon.tsx
@@ -5,9 +5,9 @@ const ErrorIcon = () => {
       viewBox='0 0 24 24'
       fill='none'
       stroke='currentColor'
-      stroke-width='2'
-      stroke-linecap='round'
-      stroke-linejoin='round'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
     >
       <polygon points='7.86 2 16.14 2 22 7.86 22 16.14 16.14 22 7.86 22 2 16.14 2 7.86 7.86 2'></polygon>
       <line x1='12' y1='8' x2='12' y2='12'></line>

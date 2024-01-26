b6c3754a4a217758bcd1dff30555ee10bca1aa8a
ssaavedraa <ssaavedraa@santiagosaavedra.com.co>
2024-01-26 16:30:54 -0500

    change theme background

--- hex-store-migration/src/app/admin/layout.tsx
+++ hex-store-migration/src/app/admin/layout.tsx
@@ -4,12 +4,7 @@ const AdminPanelLayout: React.FC<{ children: ReactNode }> = ({ children }) => {
   return (
     <main className='w-screen h-screen flex flex-col justify-center items-center'>
       <div className='w-5/6 h-5/6 flex gap-x-4'>
-        <aside
-          className='bg-[#343346] bg-opacity-50 w-2/12 h-full rounded-lg'
-          style={{
-            boxShadow: '28px 28px 57px #1e1d29, -28px -28px 57px #282737',
-          }}
-        />
+        <aside className='bg-[#343346] bg-opacity-50 w-2/12 h-full rounded-lg shadow-neumorphic' />
         <section className='w-10/12 bg-[#343346] bg-opacity-50 rounded-lg p-4 overflow-clip shadow-neumorphic'>
           {children}
         </section>
--- hex-store-migration/tailwind.config.ts
+++ hex-store-migration/tailwind.config.ts
@@ -16,14 +16,24 @@ const config: Config = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       boxShadow: {
-        neumorphic: '28px 28px 57px #1e1d29, -28px -28px 57px #282737',
-        'neumorphic-sm': '4px 4px 9px #100f16, -4px -4px 9px #36354a',
+        neumorphic: '28px 28px 57px #0C001A, -28px -28px 57px #2A0057',
+        'neumorphic-sm': '4px 4px 9px #0C001A, -4px -4px 9px #2A0057',
         'neumorphic-inset':
           'inset 3px 3px 6px #1e1d29, inset -3px -3px 16px #282737',
       },
     },
   },
   darkMode: 'class',
-  plugins: [nextui()],
+  plugins: [
+    nextui({
+      themes: {
+        dark: {
+          colors: {
+            background: '#1B0038',
+          },
+        },
+      },
+    }),
+  ],
 }
 export default config

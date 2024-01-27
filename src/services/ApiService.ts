01d6fb0ea6110a2f18973479e9eae6cdd1b2718d
ssaavedraa <ssaavedraa@santiagosaavedra.com.co>
2024-01-27 03:47:22 -0500

    migrated project to Nextjs

--- src/services/ApiService.ts
+++ src/services/ApiService.ts
@@ -3,13 +3,13 @@ const { BACKEND_URL } = process.env
 export default class ApiService {
   public async getData<T>(endpoint: string): Promise<T> {
     try {
-      const response = await fetch(`${BACKEND_URL}/${endpoint}`)
+      const response = await fetch('https://api.escuelajs.co/api/v1/products')
 
       const data: T = await response.json()
 
       return data
     } catch (error) {
-      console.error(error)
+      console.error({ error })
       return {} as T
     }
   }

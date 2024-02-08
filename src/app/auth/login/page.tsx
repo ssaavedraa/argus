4c1f29c5287b014849e0565612b34d3ee8d88255
ssaavedraa <ssaavedraa@santiagosaavedra.com.co>
2024-02-08 13:13:12 -0500

    fixed build error

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import LoginPageForm from '@/pages/login/LoginPageForm'
+import LoginPageForm from '@/views/login/LoginPageForm'
 import { loginUser } from './actions'
 
 export default function LoginPage() {
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,7 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/views/**/*.{js,ts,jsx,tsx,mdx}',
     './src/ui/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',

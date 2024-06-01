5ce3797d442ff8d84e755434aab2575696a5807c
Santiago Saavedra <ssaavedraa@santiagosaavedra.com.co>
2024-06-01 19:07:38 +1000

    removed debugging

--- src/pages/admin/Dashboard/Sidebar/UserCard/actions.ts
+++ src/pages/admin/Dashboard/Sidebar/UserCard/actions.ts
@@ -5,7 +5,6 @@ import { cookies } from 'next/headers'
 export async function getUser() {
   const cookieStore = cookies()
   const session = cookieStore.get('session')?.value
-  console.debug('🚀 ~ file: actions.ts:8 ~ getUser ~ session:', session)
 
   const baseUrl = process.env.NEXT_PUBLIC_FRONTEND_BASE_URL
 

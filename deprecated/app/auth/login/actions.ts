a85f4fa5086500c5b84a00bd182bab293e6c8568
Santiago Saavedra <ssaavedraa@santiagosaavedra.com.co>
2024-06-10 07:03:24 +1000

    deprecated previous architecture

---
+++ deprecated/app/auth/login/actions.ts
@@ -0,0 +1,76 @@
+// 'use server'
+
+// interface RawFormData {
+//   email: FormDataEntryValue
+//   password: FormDataEntryValue | null
+// }
+
+// export async function loginUser(
+//   state: any,
+//   formData: FormData,
+// ): Promise<void | {
+//   message: string
+//   isSuccess: boolean
+//   isFailed: boolean
+// }> {
+//   try {
+//     const rawFormData: RawFormData = {
+//       email: (formData.get('email')?.toString() || '').toLowerCase(),
+//       password: formData.get('password'),
+//     }
+
+//     const baseUrl = process.env.NEXT_PUBLIC_FRONTEND_BASE_URL
+
+//     const response = await fetch(`${baseUrl}/cms/auth/login`, {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify(rawFormData),
+//       credentials: 'include',
+//     })
+
+//     // const setCookies = response.headers.getSetCookie() || []
+
+//     // const sessionCookie = setCookies
+//     //   .find((cookie) => cookie.includes('session'))
+//     //   ?.split('=')
+
+//     const responseData: {
+//       status: number
+//       message: string
+//     } = await response.json()
+
+//     if (response?.status?.toString().match(/\b(?:4\d{2}|5\d{2})\b/)) {
+//       return {
+//         ...state,
+//         message: responseData.message,
+//         isFailed: true,
+//         isSuccess: false,
+//       }
+//     }
+
+//     if (response.status === 201) {
+//       // const oneWeekFromNow = new Date()
+//       // oneWeekFromNow.setDate(oneWeekFromNow.getDate() + 7)
+
+//       // if (sessionCookie) {
+//       //   cookies().set(sessionCookie[0], sessionCookie[1], {
+//       //     httpOnly: true,
+//       //     secure: true,
+//       //     sameSite: true,
+//       //     expires: oneWeekFromNow,
+//       //   })
+//       // }
+
+//       return {
+//         ...state,
+//         message: responseData?.message,
+//         isFailed: false,
+//         isSuccess: true,
+//       }
+//     }
+//   } catch (error) {
+//     console.error(error)
+//   }
+// }

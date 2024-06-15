713ad9b27e8d1617b933c78767d6c0845c15db4b
Santiago Saavedra <ssaavedraa@santiagosaavedra.com.co>
2024-06-15 23:34:12 +1000

    fixed required suspense

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,10 +1,15 @@
+import { Suspense } from 'react'
+
 import { login } from '@hex-actions'
 
 import LoginPage from '@hex-pages/auth/Login'
 
-
 const AuthLogin = () => {
-  return <LoginPage action={login} />
+  return (
+    <Suspense>
+      <LoginPage action={login} />
+    </Suspense>
+  )
 }
 
 export default AuthLogin

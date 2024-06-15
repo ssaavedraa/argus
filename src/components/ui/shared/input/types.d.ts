8158cba5ea39db09bd33c2d135dff57e052423a8
Santiago Saavedra <ssaavedraa@santiagosaavedra.com.co>
2024-06-15 23:26:58 +1000

    removed unused files

--- src/components/ui/shared/input/types.d.ts
+++
@@ -1,8 +0,0 @@
-import { ChangeEvent, InputHTMLAttributes } from 'react';
-
-export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-  // eslint-disable-next-line no-unused-vars
-  handleChange: (e: ChangeEvent<HTMLInputElement>) => void
-  label: string
-  capture?: boolean | 'user' | 'environment';
-}

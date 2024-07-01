30e4782133a6c31026694faf5927213226989284
Santiago Saavedra <ssaavedraa@santiagosaavedra.com.co>
2024-07-01 11:08:31 +1000

    refactor(buton-component): changed logout button to use hex button

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,6 +1,8 @@
 import { signOut } from '@hex-auth'
 import { publicRoutes } from '@hex-routes'
 
+import { Button } from '@hex-shared-components/button'
+
 const DashboardPage = async () => {
   // TODO: COMPONENTIZE HEADER
   return (
@@ -15,7 +17,7 @@ const DashboardPage = async () => {
           })
         }}
       >
-        <button>Sign Out</button>
+        <Button>Sign Out</Button>
       </form>
     </>
   )
